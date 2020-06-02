a81c2e8438cd3a2550c0206651043d3c256a8470
Diego Rustnnes Bandeira <diego.rustnnes@conductor.com.br>
2020-06-02 04:16:45 -0300

    Java > extra-ex7: Raindrops

---
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,10 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        String result = "";
+        if(number % 3 == 0) result += "Pling";
+        if(number % 5 == 0) result += "Plang";
+        if(number % 7 == 0) result += "Plong";
+        return result.isEmpty() ? Integer.toString(number): result;
+    }
+}
